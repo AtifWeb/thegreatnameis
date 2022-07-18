2289ae41eb558fd3df524c4c18200bff4780e0a6
Atif Asim <you@example.com>
2022-07-18 12:13:06 -0700

    new

--- src/components/CardsWrapper.jsx
+++ src/components/CardsWrapper.jsx
@@ -1,5 +1,14 @@
 import React from "react";
 import { Splide, SplideSlide } from "@splidejs/react-splide";
+// Default theme
+import "@splidejs/react-splide/css";
+
+// or other themes
+import "@splidejs/react-splide/css/skyblue";
+import "@splidejs/react-splide/css/sea-green";
+
+// or only core styles
+import "@splidejs/react-splide/css/core";
 import { CardSplide } from "./CardSplide";
 export const CardsWrapper = () => {
   return (
@@ -8,9 +17,10 @@ export const CardsWrapper = () => {
         className="CardsWrapperSplider"
         aria-label="My Favorite Images"
         options={{
-          perPage: 2,
+          perPage:1,
           gap: 20,
           arrows: false,
+        
         }}
       >
         <SplideSlide>
